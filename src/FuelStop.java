f56dea9562e96492b25f67a8976aafc263d6c72f
jalaverdiyeva <93456693+jalaverdiyeva@users.noreply.github.com>
2025-11-29 12:32:54 +0400

    FuelStop.java

--- src/FuelStop.java
+++ src/FuelStop.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FuelStop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int yanacaqSeviyyesi=20; //başlanğıc yanacaq səviyyəsi
+
+        while(true) { //sonsuz dövr
+            //cari yanacaq səviyyəsini çap edirik
+            System.out.println("Cari yanacaq səviyyəsi: "+yanacaqSeviyyesi);
+
+            //sürücüdən yanacaq miqdarını soruşuruq
+            System.out.print("Neçə litr yanacaq əlavə etdiniz? ");
+            int elaveLitr=scanner.nextInt();
+
+            //yanacaq səviyyəsini artırırıq
+            yanacaqSeviyyesi+=elaveLitr;
+
+            //yoxlayırıq ki, hədəfə çatıb-çatmadığını
+            if (yanacaqSeviyyesi >= 80) {
+                System.out.println("Yanacaq hədəfə çatdı. Səfərə davam!");
+                break; //dövrü dayandırırıq
+            } else {
+                System.out.println("Yanacaq yenə də azdır. Növbəti məntəqəyə gedirik...");
+            }
+        }
+
+        scanner.close();
+    }
 }
